c46e35a7b400a936d97eed1e223137ead4811705
yasir900aslam <yasir900aslam@gmail.com>
2023-03-12 02:36:49 +0500

    feat: submit service completed

---
+++ src/services/submit/submit.service.ts
@@ -0,0 +1,48 @@
+import { ApiServiceBase } from '../base.service';
+import FormData from 'form-data';
+import * as path from 'path';
+import { SubmitRepresentationResponse, SubmitCodeRepresentationPayload } from '../../types';
+
+export class SubmitService extends ApiServiceBase {
+  /**
+   * @param relativePath Relative Path of the file
+   * @param fileContent Buffer Contents of the txt file
+   * @param filePath Path of the file
+   */
+  async submitTextFile(relativePath: string, fileContent: any, filePath: string) {
+    const formData = new FormData();
+    formData.append(relativePath, fileContent, { filename: path.basename(filePath) });
+
+    const response = await this.post<SubmitRepresentationResponse>('/submit', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    return response;
+  }
+
+  /**
+   * @param codeRepresentation The Code Representation of the file
+   */
+  async submitCodeFile(codeRepresentation: SubmitCodeRepresentationPayload) {
+    const response = await this.post<SubmitRepresentationResponse>('/submit', codeRepresentation, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return response;
+  }
+
+  /**
+   * Fetches the job status and results for the given job ID.
+   * If no job ID is provided, returns the latest results for the session if available.
+   * Returns a Promise that resolves to a JobStatus object.
+   * @param jobId The Job Id of the submission
+   */
+  async getJobStatus(jobId?: string) {
+    const url = `/analysis${jobId ? `?job=${jobId}` : ''}`;
+    const response = this.post<SubmitRepresentationResponse>(url);
+    return response;
+  }
+}
--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,43 @@
-interface ApiResponse {
+export interface SubmitRepresentationResponse {
   jobId: string;
-  status: string;
-  results?: {
-    id: string;
-    path: string;
+  status: 'complete' | 'pending' | 'running' | 'failed';
+  results?: Problem[];
+}
+
+export interface Problem {
+  id: string;
+  path: string;
+  startLine: number;
+  endLine: number;
+  category: string;
+  summary: string;
+  description: string;
+}
+
+interface Identity {
+  [id: string]: {
+    language: string;
+    filePath: string;
     startLine: number;
     endLine: number;
-    category: string;
-    summary: string;
-    description: string;
-  }[];
+    text: any;
+  };
+}
+
+interface Node {
+  [id: string]: Array<{
+    type: 'FILE';
+    identity: string;
+  }>;
+}
+
+interface Edge {
+  [id: string]: any[];
+}
+
+export interface SubmitCodeRepresentationPayload {
+  format: 'full' | 'partial';
+  identities: Identity;
+  nodes: Node;
+  edges: Edge;
 }
