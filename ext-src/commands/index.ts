eb1e7a51abb7b2eabd403e0e9776d47d5c5397d2
Yasir Aslam <Yasir900aslam@gmail.com>
2024-01-30 11:05:14 +0500

    fix: removed commands folder to fix duplication of files in git tree

--- ext-src/commands/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './AnalyzeDocument';
-export * from './DetailDocument';
-export * from './DiscardSuggestion';
-export * from './EndorseSuggestion';
-export * from './FixSuggestion';
-export * from './FocusRecommendation';
