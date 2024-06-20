e7dca4dee6454b7bf6c6244731704069f6ae490d
Avi Gopal <avi@metabob.com>
2024-06-20 14:54:08 -0700

    another location

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -126,10 +126,7 @@ const AccountSettingProvider = ({ children }: Props): JSX.Element => {
           }
 
           if (currentFile) {
-            const filename: string | undefined = currentFile?.fileName
-              ?.split('/')
-              ?.pop()
-              ?.replace('.git', '');
+            const filename: string | undefined = payload?.uri?.fsPath;
 
             if (!filename) {
               setCurrentEditor(undefined);
