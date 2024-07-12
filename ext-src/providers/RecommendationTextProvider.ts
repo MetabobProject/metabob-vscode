bf58e60d545901a8e8ec356d26e86c663fd3529d
Yong Lin <yonglintan@gmail.com>
2024-07-11 23:19:03 -0700

    fix start line off by one when replacing original text with recommendation

--- ext-src/providers/RecommendationTextProvider.ts
+++ ext-src/providers/RecommendationTextProvider.ts
@@ -1,22 +1,30 @@
-import { TextDocumentContentProvider, Uri, workspace } from "vscode";
+import { TextDocumentContentProvider, Uri, workspace } from 'vscode';
 
 export class RecommendationTextProvider implements TextDocumentContentProvider {
   async provideTextDocumentContent(uri: Uri): Promise<string | null | undefined> {
     const currDocumentText = (await workspace.openTextDocument(Uri.file(uri.path))).getText();
-    const {recommendation, startLine, endLine} = JSON.parse(uri.query) as {recommendation: string, startLine: number, endLine: number};
+    const { recommendation, startLine, endLine } = JSON.parse(uri.query) as {
+      recommendation: string;
+      startLine: number;
+      endLine: number;
+    };
 
-    return this._replaceLines(currDocumentText, startLine, endLine, recommendation)
+    return this._replaceLines(currDocumentText, startLine, endLine, recommendation);
   }
 
   /** Replaces the lines from startLine to endLine in documentText with newContent. */
-  private _replaceLines(documentText: string, startLine: number, endLine: number, newContent: string): string {
+  private _replaceLines(
+    documentText: string,
+    startLine: number,
+    endLine: number,
+    newContent: string,
+  ): string {
     // remove the last newline character since it gets added by the .join('\n')
     newContent = newContent.replace(/\n$/, '');
 
     const lines = documentText.split('\n');
-    const newLines = lines.slice(0, startLine - 1).concat([newContent]).concat(lines.slice(endLine))
+    const newLines = lines.slice(0, startLine).concat([newContent]).concat(lines.slice(endLine));
 
-    return newLines.join('\n')
+    return newLines.join('\n');
   }
-
 }
